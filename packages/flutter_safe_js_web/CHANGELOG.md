97ff051bc1410a7b39e379a49be3c79f8811fd21
Emmanuel VAÏSSE <emmanuel.vaisse@boursorama.fr>
2022-07-11 18:14:43 +0200

    fix(plugin): fix missing web plugin instance

--- packages/flutter_safe_js_web/CHANGELOG.md
+++ packages/flutter_safe_js_web/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.2
+
+ * add `flutter_web_plugins` package as required
+
 # 0.0.1
 
  * First version
--- packages/flutter_safe_js_web/lib/flutter_safe_js_web.dart
+++ packages/flutter_safe_js_web/lib/flutter_safe_js_web.dart
@@ -4,10 +4,10 @@ import 'dart:js' as js;
 import 'package:flutter_safe_js_platform_interface/flutter_safe_js_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-class FlutterSafeJsPlugin extends FlutterSafeJsPlatformInterface {
+class FlutterSafeJsWeb extends FlutterSafeJsPlatformInterface {
   /// Registers this class as the default instance of [FlutterSafeJsPlugin].
   static void registerWith(Registrar registrar) {
-    FlutterSafeJsPlatformInterface.instance = FlutterSafeJsPlugin();
+    FlutterSafeJsPlatformInterface.instance = FlutterSafeJsWeb();
   }
 
   @override
