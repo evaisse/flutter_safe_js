f65664caa505f27697b574deae591b6d1f766636
Emmanuel VAÏSSE <emmanuel.vaisse@boursorama.fr>
2022-07-11 17:56:58 +0200

    feat(plugin): first implementation

---
+++ packages/flutter_safe_js/CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.0.2
+
+ * add basic implementation
+
+# 0.0.1
+
+ * first version for plugin facade 
--- packages/flutter_safe_js/lib/flutter_safe_js.dart
+++ packages/flutter_safe_js/lib/flutter_safe_js.dart
@@ -1,1 +1,15 @@
+import 'package:flutter_safe_js_platform_interface/flutter_safe_js_platform_interface.dart';
 
+class FlutterSafeJs {
+  Future<void> callMethod(String javascriptFunctionName, List arguments) {
+    return FlutterSafeJsPlatformInterface.instance.callMethod(javascriptFunctionName, arguments);
+  }
+
+  Future<void> deleteProperty(String propertyName) {
+    return FlutterSafeJsPlatformInterface.instance.deleteProperty(propertyName);
+  }
+
+  Future<bool> hasProperty(String propertyName) {
+    return FlutterSafeJsPlatformInterface.instance.hasProperty(propertyName);
+  }
+}
